975ba8f23112a8355b57c262b998d7b9e20ac1a8
Matthias Bock <mail@matthiasbock.net>
2018-05-13 20:23:21 +0200

    Implemented command line argument parsing with getopt()

--- src/main.cpp
+++ src/main.cpp
@@ -8,15 +8,61 @@
 
 using namespace std;
 
+
 string devicePath = "/dev/input/js0";
 string mqtt_id = "joystick-mqtt-service";
 string mqtt_topic = "joystick/cyborg3d/";
 string mqtt_host = "localhost";
 int mqtt_port = 1883;
+bool daemonize = false;
 
 
-int main()
+void parseArgs(int argc, char** argv)
 {
+    int c;
+    while ((c = getopt(argc, argv, "f:n:t:h:p:d")) != -1)
+    {
+        switch (c)
+        {
+            case 'f':
+                devicePath = string(optarg);
+                break;
+            case 'n':
+                mqtt_id = string(optarg);
+                break;
+            case 't':
+                mqtt_topic = string(optarg);
+                break;
+            case 'h':
+                mqtt_host = string(optarg);
+                break;
+            case 'p':
+                mqtt_port = atoi(optarg);
+                break;
+            case 'd':
+                daemonize = true;
+                break;
+            case '?':
+//                if (optopt == 'c')
+//                    fprintf(stderr, "Option -%c requires an argument.\n",
+//                            optopt);
+//                else if (isprint(optopt))
+//                    fprintf(stderr, "Unknown option `-%c'.\n", optopt);
+//                else
+//                    fprintf(stderr, "Unknown option character `\\x%x'.\n",
+//                            optopt);
+                exit(0);
+            default:
+                abort();
+        }
+    }
+}
+
+
+int main(int argc, char** argv)
+{
+    parseArgs(argc, argv);
+
     // Create joystick object
     Joystick* joystick = new Joystick();
     if (!joystick->open(devicePath.c_str()))
